2b5535c6b43e99f77d7ad0beebb566c4a874aa47
aboutamritanshu <132550606+aboutamritanshu@users.noreply.github.com>
2024-02-28 22:09:17 +0530

    Day 3

---
+++ Reverse_a_String.java
@@ -0,0 +1,18 @@
+class Reverse
+{
+    public static String reverseWord(String str)
+    {
+        char[] charArray = str.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        while (start < end) {
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+            
+            start++;
+            end--;
+        }
+        return new String(charArray);
+    }
+}
