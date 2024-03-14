4a73700af724dd4fdad72fa9685a4b0facc562f6
aboutamritanshu <132550606+aboutamritanshu@users.noreply.github.com>
2024-03-14 19:49:05 +0530

    Day 17

---
+++ Reverse_array_in_groups.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    void reverseInGroups(ArrayList<Integer> arr, int n, int k) {
+        for (int i = 0; i < n; i += k) {
+            int left = i;
+            int right = Math.min(i + k - 1, n - 1);
+            while (left < right) {
+                int temp = arr.get(left);
+                arr.set(left, arr.get(right));
+                arr.set(right, temp);
+                left++;
+                right--;
+            }
+        }
+    }
+}
