edac3985d8c11d9a24f1f59c81d22960e436fbcb
aboutamritanshu <132550606+aboutamritanshu@users.noreply.github.com>
2024-03-15 21:37:07 +0530

    Day 18

---
+++ Rotate_Array.java
@@ -0,0 +1,24 @@
+class Solution
+{
+    static void reverse(int arr[], int start, int end)
+    {
+        while (start < end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void rotateArr(int arr[], int d, int n)
+    {
+        d = d % n;
+        if (d == 0) return;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+}
