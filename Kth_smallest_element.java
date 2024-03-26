0d2e5e3c5d3efb748a2de37ed214f97e9b463c01
aboutamritanshu <132550606+aboutamritanshu@users.noreply.github.com>
2024-03-26 18:48:18 +0530

    Day 30

---
+++ Kth_smallest_element.java
@@ -0,0 +1,40 @@
+class Solution {
+    public static int kthSmallest(int[] arr, int l, int r, int k) {
+        if (k > 0 && k <= r - l + 1) {
+            int pos = randomPartition(arr, l, r);
+            
+            if (pos - l == k - 1)
+                return arr[pos];
+            if (pos - l > k - 1)
+                return kthSmallest(arr, l, pos - 1, k);
+            return kthSmallest(arr, pos + 1, r, k - pos + l - 1);
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    private static int randomPartition(int[] arr, int l, int r) {
+        int n = r - l + 1;
+        Random random = new Random();
+        int pivot = random.nextInt(n);
+        swap(arr, l + pivot, r);
+        return partition(arr, l, r);
+    }
+
+    private static int partition(int[] arr, int l, int r) {
+        int x = arr[r], i = l;
+        for (int j = l; j <= r - 1; j++) {
+            if (arr[j] <= x) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, r);
+        return i;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
