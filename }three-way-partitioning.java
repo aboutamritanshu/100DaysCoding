8c8353d308007e30315cfdac31e7f60a683c6d3e
aboutamritanshu <132550606+aboutamritanshu@users.noreply.github.com>
2024-04-21 23:26:28 +0530

    Day 53

---
+++ }three-way-partitioning.java
@@ -0,0 +1,25 @@
+class Solution{
+
+    public void threeWayPartition(int array[], int a, int b)
+    {
+        int n = array.length;
+        int start = 0, end = n - 1;
+        for (int i = 0; i <= end;) {
+            if (array[i] < a) {
+
+                int temp = array[i];
+                array[i] = array[start];
+                array[start] = temp;
+                start++;
+                i++;
+            } else if (array[i] > b) {
+
+                int temp = array[i];
+                array[i] = array[end];
+                array[end] = temp;
+                end--;
+            } else {
+                i++;
+            }
+        }
+    }
