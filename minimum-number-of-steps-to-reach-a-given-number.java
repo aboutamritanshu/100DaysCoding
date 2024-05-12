de18618271e047b9c1deeda50a370cdc4bbcb290
aboutamritanshu <132550606+aboutamritanshu@users.noreply.github.com>
2024-05-12 22:22:27 +0530

    Day 73

---
+++ minimum-number-of-steps-to-reach-a-given-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    static int minSteps(int d) {
+        for(int i = 1; i < 2 * d; i++){
+            int ap = (i * (i + 1)) / 2;
+            if((ap + d) % 2 == 0 && ap >= d) return i;
+        }
+        return 2 * d;
+    }
+}
